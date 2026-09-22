0cb4c8061fcba397dce2c1139594255af87973fb
Revathi Mangamuri <revathimangamuri99@gmail.com>
2026-09-22 14:49:37 +0530

    Add files via upload

---
+++ renu.js/find.js
@@ -0,0 +1,6 @@
+let numbers = [10, 20, 30, 40, 50];
+let result = numbers = numbers.find(function(num)
+{
+    return num > 25;
+});
+console.log(result);
--- renu.js/reduce.js
+++ renu.js/reduce.js
@@ -1,4 +1,5 @@
 let numbers = [10, 20, 30, 40];
 let sum   = numbers.reduce(function(total, num) {
     return total + num;
-}, 0);
+}, 0)
+console.log(sum);
